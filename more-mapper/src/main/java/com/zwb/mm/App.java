20ab3c777711710dfc8f7d05b1d9bbe9ebb33e5d
Your Name <you@example.com>
2018-10-09 14:21:07 +0800

    Initial commit

---
+++ more-mapper/src/main/java/com/zwb/mm/App.java
@@ -0,0 +1,13 @@
+package com.zwb.mm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ more-web/src/main/java/com/zwb/mm/moreweb/MoreWebApplication.java
@@ -0,0 +1,12 @@
+package com.zwb.mm.moreweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoreWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoreWebApplication.class, args);
+	}
+}
